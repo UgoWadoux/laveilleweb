cee92fd6b397a128776d8c238a33466514d53cc8
anthonypelaz <antho230104@gmail.com>
2025-05-07 10:07:52 +0200

    Add homepage + reservation page

---
+++ components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="header bg-gray-100 h-24 flex items-center justify-between">
+    <p>Logo</p>
+    <nav>
+      <ul class="flex items-center justify-between flex-row gap-x-2">
+        <NuxtLink to="/" >
+          <span>Home</span>
+        </NuxtLink>
+        <NuxtLink to="/reservation" >
+          <span>Reservation</span>
+        </NuxtLink>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/eslint']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/eslint'],
+  imports: {
+    autoImport: true
+  }
 })
